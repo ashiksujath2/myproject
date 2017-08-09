cc0245de053491172ed8a968240372d7498c2497
Ashik Sujath <ashiksujath2@gmail.com>
2017-08-09 15:46:12 +0530

    selenium tests

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,7 +1,7 @@
 import time
 from django.test import TestCase
 from django.test import LiveServerTestCase
-# from selenium import webdriver
+from selenium import webdriver
 from django.contrib.auth.models import User
 
 
@@ -14,23 +14,23 @@ class SimpleTest(TestCase):
         self.assertEqual(1 + 1, 2)
 
 
-# class AdminTestCase(LiveServerTestCase):
+class AdminTestCase(LiveServerTestCase):
 
-#     def setUp(self):
-#         self.driver = webdriver.Firefox()
-#         User.objects.create_superuser('admin', 'foo@bar.com', 'password123')
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        User.objects.create_superuser('admin', 'foo@bar.com', 'password123')
 
-#     def tearDown(self):
-#         self.driver.quit()
+    def tearDown(self):
+        self.driver.quit()
 
-#     def test_login(self):
-#         username = "admin"
-#         password = "password123"
-#         self.driver.get('%s%s' % (self.live_server_url, '/admin/'))
-#         username_field = self.driver.find_element_by_name("username")
-#         username_field.send_keys(username)
-#         password_field = self.driver.find_element_by_name("password")
-#         password_field.send_keys(password)
-#         time.sleep(2)
-#         password_field.submit()
-#         time.sleep(1)
+    def test_login(self):
+        username = "admin"
+        password = "password123"
+        self.driver.get('%s%s' % (self.live_server_url, '/admin/'))
+        username_field = self.driver.find_element_by_name("username")
+        username_field.send_keys(username)
+        password_field = self.driver.find_element_by_name("password")
+        password_field.send_keys(password)
+        time.sleep(2)
+        password_field.submit()
+        time.sleep(1)
