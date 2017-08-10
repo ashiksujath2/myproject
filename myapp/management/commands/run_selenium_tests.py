0a65267d90d017d47a2861b166a4db2bf7fa5ac7
Ashik Sujath <ashiksujath2@gmail.com>
2017-08-10 10:44:50 +0530

    assert fail

--- myapp/management/commands/run_selenium_tests.py
+++ myapp/management/commands/run_selenium_tests.py
@@ -8,12 +8,13 @@ class Command(BaseCommand):
 
     def handle(self, **options):
         print("\nRuning Selenium Tests..!")
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome('/Users/ashik/Desktop/chromedriver')
         self.driver.get('%s%s' % ('http://127.0.0.1:8000', '/admin/'))
         username_field = self.driver.find_element_by_name("username")
         username_field.send_keys("username")
         password_field = self.driver.find_element_by_name("password")
         password_field.send_keys("password")
         time.sleep(2)
+        assert username_field.get_attribute('maxlength') == '255'
         self.driver.quit()
         print("\nDone!")
